26620d96825bbe9aa9ea3075c6c267d724ea6c54
Leukino <miquelqg99@gmail.com>
2020-10-27 10:17:27 +0100

    IMGUI FUNCIONA Pog
    estic feliç

--- EngineQT/ModulePlayer.cpp
+++ EngineQT/ModulePlayer.cpp
@@ -22,8 +22,8 @@
 #pragma comment (lib, "glew/lib/release/Win32/glew32.lib")
 #pragma comment( lib, "glew/lib/release/Win32/glew32s.lib")
 
-#include "imgui/imgui_impl_sdl.h"
-#include "imgui/imgui.h"
+//#include "imgui/imgui_impl_sdl.h"
+//#include "imgui/imgui.h"
 
 
 ModulePlayer::ModulePlayer(Application* app, bool start_enabled) : Module(app, start_enabled)
@@ -39,10 +39,12 @@ bool ModulePlayer::Start()
 	LOG("Loading player");
 	vec3 direction(0,-1,0);
 	vec3 axis(-1,0,0);
+
+	IMGUI_CHECKVERSION();
 	ImGui::CreateContext();
-	ImGuiContext* context =  ImGui::GetCurrentContext();
-	ImGui::NewFrame();
-	ImGui::Begin("Dear ImGui Style Editor", false);
+	//ImGuiContext* context =  ImGui::GetCurrentContext();
+	//ImGui::NewFrame();
+	//ImGui::Begin("Dear ImGui Style Editor", false);
 	//ImGui::SetNextWindowPos(ImVec2(650, 20), ImGuiCond_FirstUseEver);
 	//ImGui::SetNextWindowSize(ImVec2(550, 680), ImGuiCond_FirstUseEver);
 
@@ -75,7 +77,7 @@ bool ModulePlayer::CleanUp()
 // Update: draw background
 update_status ModulePlayer::Update(float dt)
 {
-	ImGui::NewFrame();
+	//ImGui::NewFrame();
 	char title[80];
 	sprintf_s(title, "Game engine");
 	App->window->SetTitle(title);
